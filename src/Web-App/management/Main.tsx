546c030bdd8434b2dbe7d2e1637fe622b2fceaa1
shahzaib khan <shahzaibshazzo@gmail.com>
2025-10-21 13:05:25 +0500

    add files

--- src/Web-App/management/Main.tsx
+++ src/Web-App/management/Main.tsx
@@ -7,10 +7,11 @@ function Main() {
   return (
     <div className="w-[83vw] h-[99vh] bg-[#0f0f0f] text-white overflow-y-auto">
       <Main_Navbar />
+      
       <Routes>
         <Route path="project" element={<Projects />} />
         <Route path="board" element={<Board />} />\
-        
+
 
       </Routes>
     </div>
