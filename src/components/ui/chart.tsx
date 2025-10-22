847a7925e1514cffea367ad7c17559bf3115225c
shahzaib khan <shahzaibshazzo@gmail.com>
2025-10-22 15:26:09 +0500

    ui changes

---
+++ src/components/ui/chart.tsx
@@ -0,0 +1,46 @@
+
+import * as React from "react"
+import * as RechartsPrimitive from "recharts"
+import { cn } from "@/lib/utils"
+
+const THEMES = { light: "", dark: ".dark" } as const
+
+export type ChartConfig = {
+  [key: string]: {
+    label?: React.ReactNode
+    icon?: React.ComponentType
+    color?: string
+  }
+}
+
+const ChartContext = React.createContext<{ config: ChartConfig } | null>(null)
+
+export function useChart() {
+  const context = React.useContext(ChartContext)
+  if (!context) throw new Error("useChart must be used within a ChartContainer")
+  return context
+}
+
+export const ChartContainer = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentProps<"div"> & {
+    config: ChartConfig
+    children: React.ReactNode
+  }
+>(({ className, children, config, ...props }, ref) => {
+  return (
+    <ChartContext.Provider value={{ config }}>
+      <div
+        ref={ref}
+        className={cn("relative flex aspect-video justify-center text-xs", className)}
+        {...props}
+      >
+        <RechartsPrimitive.ResponsiveContainer>{children}</RechartsPrimitive.ResponsiveContainer>
+      </div>
+    </ChartContext.Provider>
+  )
+})
+ChartContainer.displayName = "ChartContainer"
+
+export const ChartTooltip = RechartsPrimitive.Tooltip
+export const ChartLegend = RechartsPrimitive.Legend
